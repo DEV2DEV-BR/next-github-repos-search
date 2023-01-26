aa3af47158aee20dbf9f372a3690d3c9c973b0a2
Daniel <danieldeandradelopes@gmail.com>
2023-01-26 14:49:42 -0300

    import fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Mulish } from "@next/font/google";
+
+const mulish = Mulish({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={mulish.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
