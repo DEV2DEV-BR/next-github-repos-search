062090d2c728217d06e407d108fc6fbceaaeaca1
Daniel <danieldeandradelopes@gmail.com>
2023-01-26 17:44:13 -0300

    create formatter

--- src/utils/formatters.ts
+++ src/utils/formatters.ts
@@ -1,3 +1,26 @@
+import { RepositoryType } from "@/Types/RepositoryType";
+
+export function formatRepoName(
+  repositories: Array<RepositoryType>
+): Array<RepositoryType> {
+  for (const repository of repositories) {
+    const names = repository.name.split("-");
+
+    let formattedName = "";
+
+    for (const name of names) {
+      if (!name) {
+        continue;
+      }
+      formattedName += `${capitalize(name)} `;
+    }
+
+    repository.name = formattedName;
+  }
+
+  return repositories;
+}
+
 export function capitalize(word: string) {
   const firstLetter = word[0].toUpperCase();
   const restOfWord = word.slice(1, word.length);
